bf15530510ea900717c0ebfc0bc27a1610806af7
nicolae.peicuti <nicolae.peicuti@gmail.com>
2011-04-17 21:17:40 +0000

    Updated schematic + minor modifications

--- lpc_project.c
+++ lpc_project.c
@@ -22,50 +22,75 @@
 #include "lcd_ctrl.h"
 #include "tools.h"
 
+#define JC 16	//Joystick Center P0.16
+#define JU 17	//...
+#define JR 18
+#define JL 19
+#define JD 20
+#define BT 14	//Push button P0.14
+
 volatile int counter=0;	// count interrupts (for testing)
+int adc_value; 
+volatile char dir = 1; //motor direction: 1-ClockWise 0-CounterCW
+int stepper_code[] = { 0x00000000, 0x00200000, 0x00201000, 0x00001000 };
+volatile int stepper_pos;
+char jl_flag, jr_flag, ju_flag, jd_flag, jc_flag, bt_flag;
 
 void timer0matchISR(void) __irq
 {
+	IO0PIN = (IO0PIN & 0xFFDFEFFF) | stepper_code[stepper_pos];
+		
+	if(dir)
+	{
+		if (++stepper_pos == 4)
+			stepper_pos = 0;
+	}
+	else
+	{
+		if (--stepper_pos == -1)
+			stepper_pos = 3;
+	}
+	T0MR0 = 3*15000000UL/adc_value;	// --------------------problem if adc_value is too small
+	//counter++;
 	T0IR = 0x01; //clear interrupt flag
-	counter++;
-	//VICVectAddr = 0;
+	VICVectAddr = 0; //end of interrupt - update priority hardware
 }
-
+                       					
 int main(void)
 {
-	int i=0, adc_value=123; 
-	char dir = 1; //motor direction: 1-ClockWise 0-CounterCW
-	int step[] = { 0x00000000, 0x00200000, 0x00201000, 0x00001000 };
+	//int i=0;
+	int adc_temp;
 
 	IO0DIR = 0x40601000;
 	IO1DIR = 0x03FF0000; 
+	VPBDIV = 0;  // 0 -> PCLK=1/4*CCLK; 1 -> PCLK=CCLK; 2 -> PCLK=1/2*CCLK  (APBDIV in User manual)
 
-	IO0SET = 1<<30; //for LCD power
+	IO0SET = 1<<30; //for LCD backlight power
 
 	T0CTCR = 0x0; //Count control register: Timer mode
 	T0PR = 0; //Prescale register
-	T0MR0 = 3000000; //Match register 0
+	//T0MR0 = 15000000UL; //Match register 0
+	//T0MR0 = 3*15000000UL/60; //1 rot/s
+	T0MR0 = 15000000UL; //interrupt at 1 second
 	T0MCR = 0x3; //Match control register: Interrupt, Reset timer
 	T0TCR = 0x1; //Timer enabled for counting
 
 	VICVectCntl0 = 1<<5 | 4; //enable slot 0 interrupt, timer 0
-	VICVectAddr0 = (unsigned)timer0matchISR; //slot 0 ISR
+	VICVectAddr0 = (unsigned int)timer0matchISR; //slot 0 ISR
 	VICIntSelect = 0x00000000; //interrupt type 0=IRQ 1=FIQ
 	VICIntEnable = 1<<4; //enable timer 0 interrupt
 
-	lcd_command(0, 0x0C);
-    lcd_command(0, 0x38);
+	lcd_init();
     
-    lcd_s(64,"ADC: ");
-    lcd_n10(69,0,4,0);
+    lcd_puts(64,"ADC: ");
+    lcd_putnum(69,0,4,0);
 
-	lcd_s(11, "UDLRC");
+	lcd_puts(11, "UDLRC");
 	
-	VPBDIV = 0;  // 0 -> PCLK=1/4*CCLK; 1 -> PCLK=CCLK; 2 -> PCLK=1/2*CCLK  (APBDIV in User manual)
-	PCONP |= 1<<12;	//power for ADC
-	PINSEL1 |= 1<<26; //configure pin as ADC input
-	AD0CR = 0x00211004;
-	AD0CR |= 0x01000000;
+	//PCONP |= 1<<12;	//power for ADC
+	PINSEL1 = 1<<26 ; //configure pin as ADC input
+	AD0CR = 0x00211004;	  // PCLK=15MHz; ADCLK<=4.5MHz ; PCLK/ADCCLK=3.33 -> 4=CLKDIV+1 -> CLKDIV=3
+	AD0CR |= 0x01000000; //----------------------------
 
  	while(1)
 	{
@@ -78,27 +103,46 @@ int main(void)
 
 		do
 		{
-			adc_value = AD0GDR;
+			adc_temp = AD0GDR;
 		}
-		while( (adc_value & 0x80000000) == 0);
+		while( (adc_temp & 0x80000000) == 0);	//----------------------
 
-		adc_value = (adc_value >>6) & 0x03FF;
+		adc_value = (adc_temp >>6) & 0x03FF;
 		
-		lcd_n10(69, adc_value, 4, 0);
+		lcd_putnum(69, adc_value, 4, 0);
 		
-		lcd_n10(75, IOPIN0>>17&1, 1, 0); 
-		lcd_n10(76, IOPIN0>>20&1, 1, 0);
-		lcd_n10(77, IOPIN0>>19&1, 1, 0);
-		lcd_n10(78, IOPIN0>>18&1, 1, 0);
-		lcd_n10(79, IOPIN0>>16&1, 1, 0);
+		lcd_putnum(75, IOPIN0>>17&1, 1, 0); 
+		lcd_putnum(76, IOPIN0>>20&1, 1, 0);
+		lcd_putnum(77, IOPIN0>>19&1, 1, 0);
+		lcd_putnum(78, IOPIN0>>18&1, 1, 0);
+		lcd_putnum(79, IOPIN0>>16&1, 1, 0);
 
-		lcd_n10(0, counter, 4, 0);
+		//T0MR0 = 3*15000000UL/adc_value;
 
+		if (is_pressed(IO0PIN, JL, &jl_flag))
+			counter++;
+		if (is_pressed(IO0PIN, JR, &jr_flag))
+			counter++;
+		if (is_pressed(IO0PIN, JU, &ju_flag))
+			counter++;
+		if (is_pressed(IO0PIN, JD, &jd_flag))
+			counter++;
+		if (is_pressed(IO0PIN, JC, &jc_flag))
+		{
+			counter++;
+			dir = 1 - dir;
+		}
+
+		if (is_pressed(IO0PIN, BT, &bt_flag))
+			counter++;
+			
+		lcd_putnum(0, counter, 4, 0);
+		
+		//delay_ms(10); 
+/*
 		IO0PIN = (IO0PIN & 0xFFDFEFFF) | step[i];
-		delay((1023-adc_value)*4);
+		delay_us((1023-adc_value)*60);	  //48,3 Hz max signal freq for stepper
 		
-		dir = IO0PIN>>16 & 1;
-
 		if(dir)
 		{
 			if (++i == 4)
@@ -109,6 +153,6 @@ int main(void)
 			if (--i == -1)
 				i = 3;
 		}
+*/
 	}
 }
-
--- tools.c
+++ tools.c
@@ -1,8 +1,44 @@
 #include "tools.h"
 
-void delay(int nr)
+void delay_us(int nr)
 {
-	int i;
-	for( ;nr>0;nr--)
-		for(i=0;i<1000;i++);
+	for(nr*=FOR_CYCLES_1US; nr!=0; nr--);
+}
+
+void delay_ms(int nr)
+{
+	for(nr*=FOR_CYCLES_1US*1000; nr!=0; nr--);
+}
+
+char is_pressed(int port, char pin, char *flag)
+{
+	char ret = 0;
+	if( (port & (1<<pin) ) == 0 )
+	{
+		if(*flag == 0)
+		{
+			*flag = 1;
+			//delay_ms(20); //debouncing
+			ret = 1; //when button is pressed (immediately after falling edge)
+		}
+		else
+		{
+			//count ...
+			//ret = 2; //when button is hold
+		}
+	}
+	else
+	{
+		if(*flag == 1)
+		{
+			*flag = 0;
+			//delay_ms(20);	//debouncing
+			//ret = 3; //when button is released (immediately after rising edge)
+		}
+		else
+		{
+			//button is not pressed
+		}
+	}
+	return ret;
 }
